559709516a9615419e118d7ea96b3e0ae73658ae
krb3p <ryankatherineburke@gmail.com>
2017-02-27 13:56:53 -0500

    update4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "password_security"
   end
 
   get '/' do
@@ -12,18 +14,23 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets/new' do
-
     erb :'/tweets/create_tweet'
   end
 
-  post '/tweet' do
-    binding.pry
+  post '/tweets' do
+        # binding.pry
+    @tweet = Tweet.create(content: params[:tweet][:content])
+    @tweet.user = User.find(session[:id])
     erb :'tweets/show_tweet'
   end
 
   # get '/tweet' do
   #   redirect
   # end
+
+  get '/tweets' do
+    erb :'tweets/tweets'
+  end
 
 
 
