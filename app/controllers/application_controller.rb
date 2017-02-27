04cf72f09d235329457d290c66c68282f12e4702
hwalborn <hwalborn@gmail.com>
2017-02-27 12:18:04 -0500

    started login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
+  get '/login' do
+    erb :'users/login'
+  end
 
+  post '/login' do
+    erb :'tweets/tweets'
+  end
   # get '/homepage'
 
 end
